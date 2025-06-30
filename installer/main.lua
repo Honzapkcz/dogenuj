81f8d50cd2a45f3761d8db74fcc044f22d96cb46
Honzapkcz <honzapkc@gmail.com>
2025-06-30 12:35:15 +0200

    make few tui dialogs boxes

--- installer/main.lua
+++ installer/main.lua
@@ -1,7 +1,7 @@
-package.path = "modules/share/?.lua;modules/share/?/init.lua"
-package.cpath = "modules/lib/?/core.so;modules/lib/?.so"
+package.path = "share/?.lua;share/?/init.lua"
+package.cpath = "lib/?/core.so;lib/?.so"
 local term = require("terminal")
-local sock = require("socket")
+local http = require("socket.http")
 local lfs = require("lfs")
 
 local function dump(t)
@@ -10,9 +10,90 @@ local function dump(t)
     end
 end
 
-dump(term)
-print()
-dump(sock)
-print()
-dump(lfs)
-print()
+http.USERAGENT = "Kujnuj/2.3.1 (honzapkcz)"
+
+-- local body, code, headers = http.request("https://srv.hbmc.net/MODPACKVERSION")
+
+local sbox = term.draw.box_fmt.single
+local depth = 0
+
+function table_longest(t)
+    local l = 0
+    for _, v in ipairs(t) do
+        l = (#v > l) and #v or l
+    end
+    return l
+end
+
+function string_split(str, sep)
+    sep = sep or "\n"
+    local t = {}
+    for s in string.gmatch(str, "([^"..sep.."]*)") do
+        table.insert(t, s)
+    end
+    return t
+end
+
+function popup_yesno(title, question)
+    local y, x = 2 + depth * 2, 2 + depth * 4
+    depth = depth + 1
+    question = string_split(question)
+    term.cursor.position.set(y, x)
+    term.draw.box(#question + 5, table_longest(question) + 4, sbox, true, title)
+    for i, v in ipairs(question) do
+        term.cursor.position.set(y + i, 2 + x)
+        term.output.print(v)
+    end
+    term.cursor.position.set(#question + 2 + y, 1 + x)
+    term.draw.line.horizontal(table_longest(question) + 2)
+    term.cursor.position.set(#question + 3 + y, 2 + x)
+    term.output.print("Y - Yes  N - No")
+    depth = depth - 1
+    while true do
+        local c = term.input.readansi(1)
+        if c == "y" then
+            return true
+        elseif c == "n" then
+            return false
+        end
+    end
+end
+
+function popup_message(title, message)
+    local y, x = 2 + depth * 2, 2 + depth * 4
+    depth = depth + 1
+    message = string_split(message)
+    term.cursor.position.set(y, x)
+    term.draw.box(#message + 5, table_longest(message) + 4, sbox, true, title)
+    for i, v in ipairs(message) do
+        term.cursor.position.set(i + y, 2 + x)
+        term.output.print(v)
+    end
+    term.cursor.position.set(#message + 2 + y, 1 + x)
+    term.draw.line.horizontal(table_longest(message) + 2)
+    term.cursor.position.set(#message + 3 + y, 2 + x)
+    term.output.print("Any to OK")
+    depth = depth - 1
+    term.input.readansi(9999)
+end
+
+local function main_redraw()
+    local h, w = term.size()
+    term.clear.screen()
+    term.cursor.position.set(1, 1)
+    term.draw.box(h, w, term.draw.box_fmt.single, nil, nil, true)
+    term.output.flush()
+end
+
+local function main()
+    main_redraw()
+    local yn = popup_yesno("Question?", "Is this good enough popup?\nI hope so...\n\nHehehehe\nGetting delighted\noogabooga")
+    main_redraw()
+    if yn then
+        popup_message("Message", "Thank you...")
+    else
+        popup_message("Message", "Fuck you...")
+    end
+end
+
+term.initwrap(main, {displaybackup = true, filehandle = io.stdout})()
---
+++ installer/spinner.lua
@@ -0,0 +1,73 @@
+package.path = "share/?.lua;share/?/init.lua"
+package.cpath = "lib/?/core.so;lib/?.so"
+local t = require("terminal")
+local p = require("terminal.progress")
+
+
+local function main()
+  -- create one of each spinners
+  local spinners = {}
+
+  -- create room to display the spinners
+  local lst = {}
+  for name, seq in pairs(p.sprites) do
+    print("     "..name) -- create a line for display
+    lst[#lst+1] = name
+  end
+  print("                                                       <-- ticker type")
+
+  -- create all spinners with fixed positions (positions are optional)
+  local r, _ = t.cursor.position.get()
+  for i, name in ipairs(lst) do
+    local done_sprite, done_textattr
+    if i <= #lst/2 then
+      -- set done to checkmark character for the first half of the spinners
+      done_sprite = "✔  "
+      done_textattr = {fg = "green", brightness = 3}
+    end
+    spinners[#spinners+1] = p.spinner {
+      sprites = p.sprites[name],
+      col = 1,
+      row = r - #lst - 2 + i,
+      done_textattr = done_textattr,
+      done_sprite = done_sprite,
+    }
+  end
+
+
+  -- add the ticker one last
+  spinners[#spinners+1] = p.spinner {
+    -- uses utf8 character
+    sprites = p.ticker("🕓-Please wait-🎹...", 30, "Done!"),
+    col = 1,
+    row = r - 1,
+    textattr = {fg = "black", bg = "red", brightness = "normal"},
+    done_textattr = {brightness = "high"},
+  }
+
+
+  t.output.write("Press any key to stop the spinners...")
+  t.cursor.visible.set(false)
+
+
+  -- loop until key-pressed
+  while true do
+    for _, s in ipairs(spinners) do
+      s()
+    end
+    t.output.flush()
+    if t.input.readansi(0.02) then
+      break -- a key was pressed
+    end
+  end
+
+  -- mark spinners done
+  for _, s in ipairs(spinners) do
+    s(true)
+  end
+  t.cursor.visible.set(true)
+  t.output.print() -- move to new line (we're still on the 'press any key' line)
+end
+
+-- run the main function, wrapped in terminal init/shutdown
+t.initwrap(main)()
