6245d231baaf66dcb7b3802047a2f9388cf3307a
Honzapkcz <honzapkc@gmail.com>
2025-07-05 23:25:18 +0200

    make recipes for mortar and tag hammers

--- kubejs/server_scripts/gregtools.js
+++ kubejs/server_scripts/gregtools.js
@@ -7,7 +7,7 @@ const mats = global.materials
 
 ServerEvents.tags("item", e => {
     e.add("minecraft:smelts_to_glass", "betterend:endstone_dust")
-    for (const mat in hammers) {
+    for (const mat of hammers) {
         e.add("create:hammers", "create_ironworks:"+mat+"_hammer")
     }
     const pickaxes = ["minecraft:wooden_pickaxe", "minecraft:stone_pickaxe", "minecraft:iron_pickaxe",
@@ -15,12 +15,24 @@ ServerEvents.tags("item", e => {
         "create_ironworks:copper_pickaxe", "create_ironworks:bronze_pickaxe", "create_ironworks:brass_pickaxe",
         "create_ironworks:steel_pickaxe"
     ]
-    for (pick in pickaxes) {
-        e.add("crate:pickaxes", pick)
+    for (const pick of pickaxes) {
+        e.add("kubejs:pickaxes", pick)
     }
 })
 
 ServerEvents.recipes(e => {
+    e.shaped(Item.of("kubejs:mortar"),
+        [
+            " T ",
+            "STS",
+            "SSS"
+        ],
+        {
+            T: "#c:tin_ingots",
+            S: "minecraft:smooth_stone"
+        }
+    )
+
     /* PLATES */
     /*for (const mat of mats) {
         if (!mat[global.PLATE]) {
